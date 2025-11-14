cdf2c44b9c2876e4a27b9026c30c98440b37f7f3
steven-zhang <yustevenzhang@gmail.com>
2025-11-14 15:57:16 +0100

    data fetched in presentatioans page

--- app/presentations/page.tsx
+++ app/presentations/page.tsx
@@ -1,7 +1,15 @@
 
 export default async function PresetationPage() {
+    const res = await fetch(`${process.env.BASE_URL}/api/presentations`, {
+        method: 'GET',
+        // headers: {
+        //     'Content-Type': 'application/json',
+        // },
+    });
+    const data = await res.text()
+
 
     return (
-        <div>page </div>
+        <div>{data} </div>
     )
 }
