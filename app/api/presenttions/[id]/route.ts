a5dd847b52a607fd778cb9184b5240a28431595a
steven-zhang <yustevenzhang@gmail.com>
2025-11-14 15:37:10 +0100

    feature: create presentation api

---
+++ app/api/presenttions/[id]/route.ts
@@ -0,0 +1,9 @@
+export async function GET() {
+    return new Response("Hello, Presenttion page!");
+}
+export async function DELETE() {
+    return new Response("Delete a Presenttion!");
+}
+export async function PUT() {
+    return new Response("Update a Presenttion!");
+}
---
+++ app/api/presenttions/route.ts
@@ -0,0 +1,6 @@
+export async function GET() {
+    return new Response("Hello, Presenttions!");
+}
+export async function POST() {
+    return new Response("Create a new Presenttion!");
+}
