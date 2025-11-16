5a4aee51d1734aa58870094c6fdcbb0c59110d3d
steven-zhang <yustevenzhang@gmail.com>
2025-11-16 19:40:32 +0100

    feat: transformer handle works on image

--- components/contents/ImageContent.tsx
+++ components/contents/ImageContent.tsx
@@ -1,26 +1,55 @@
-import { Image } from "react-konva";
+import { Image, Transformer } from "react-konva";
 import { contentsMap } from "../globalState/contentsMap";
+import { useEffect, useRef } from "react";
+import Konva from "konva";
 type Props = {
     id: string
 }
 
 export default function ImageContent({ id }: Props) {
+    const imageRef = useRef<Konva.Image | null>(null)
+    const transformerRef = useRef<Konva.Transformer | null>(null);
 
     //get image content from contentsMap
     // if no image content, render empty div
     const content = contentsMap.get(id)
     if (!content || !content.img) return null
 
-    return (
-        <Image
-            key={content.id}
-            x={content.x}
-            y={content.y}
-            width={content.width}
-            height={content.height}
-            image={content.img}
-            draggable
+    useEffect(() => {
+        if (transformerRef.current && imageRef.current) {
+            transformerRef.current.nodes([imageRef.current]);
+            transformerRef.current.getLayer()?.batchDraw();
+        }
+    })
 
-        />
+
+    return (
+        <>
+            <Image
+                ref={imageRef}
+                key={content.id}
+                x={content.x}
+                y={content.y}
+                width={content.width}
+                height={content.height}
+                image={content.img}
+                draggable
+            />
+
+            <Transformer
+                ref={transformerRef}
+                rotateEnabled={true}
+                enabledAnchors={['top-left', 'top-right', 'bottom-left', 'bottom-right']}
+                boundBoxFunc={(oldBox, newBox) => {
+                    // prevent too small size
+                    if (newBox.width < 20 || newBox.height < 20) {
+                        return oldBox;
+                    }
+                    return newBox;
+                }}
+
+            />
+        </>
+
     )
 }
