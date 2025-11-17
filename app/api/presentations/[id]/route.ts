7c255740b7dcc16f7131054440f6e7c2770357b3
steven-zhang <yustevenzhang@gmail.com>
2025-11-17 13:18:29 +0100

    feat: add presentation delete api

--- app/api/presentations/[id]/route.ts
+++ app/api/presentations/[id]/route.ts
@@ -1,9 +1,55 @@
-export async function GET() {
-    return new Response("Hello, Presenttion page!");
+import { prisma } from "@/prisma/prisma";
+import { NextRequest } from "next/server";
+
+export async function GET(
+    request: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) {
+    const { id } = await context.params;
+    try {
+        const presentation = await prisma.presentation.findUnique({
+            where: { id },
+        })
+
+        if (!presentation) {
+            return new Response(
+                JSON.stringify({ error: 'Presentation not found' }), {
+                status: 404,
+                headers: { 'Content-Type': 'application/json' }
+            });
+        }
+
+        return new Response(
+            JSON.stringify(presentation), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    } catch (error) {
+        return new Response(
+            JSON.stringify({ error: 'Internal Server Error' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
 }
-export async function DELETE() {
-    return new Response("Delete a Presenttion!");
+export async function DELETE(
+    request: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) {
+    const { id } = await context.params;
+    if (!id) {
+        return new Response("Bad Request: Missing presentation ID", { status: 400 });
+    }
+
+    try {
+        await prisma.presentation.delete({
+            where: { id }
+        });
+        return new Response("Presentation Deleted!", { status: 200 });
+    } catch (err) {
+        return new Response("Internal Server Error", { status: 500 });
+    }
 }
 export async function PUT() {
-    return new Response("Update a Presenttion!");
+    return new Response("presentation update not works");
 }
